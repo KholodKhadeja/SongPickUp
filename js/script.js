0c3e6482cab1bf5665f3bc9c85f9283ab58aa49b
KholodKhadeja <kholod.khadeja@hotmail.com>
2022-08-15 22:37:48 +0300

    Final Version

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const addSong=document.querySelector("#addBtn");
 const songsListDiv=document.querySelector(".songList");
 const iframeContainer=document.querySelector(".rightContainer");
 const layer1 = document.querySelector(".mainContainer");
-const layer2=document.querySelector(".popupContainer");
+const layer2=document.querySelector("#popupContainer");
 const closeLaye2=document.querySelector("#ximg");
 
 const dataSource='https://docs.google.com/spreadsheets/d/1VZdbHiWNYwnCXTTxoUhcAtToloUDa8H8E66fQSmLO6U/gviz/tq?';
@@ -14,7 +14,7 @@ fetch(dataSource).then(response => response.text())
   let htmlCode=' ';
     let songsData = JSON.parse(jsonData.substr(47).slice(0,-2));
     songsData.table.rows.forEach(element => {
-      htmlCode+=`<div  class="listItem" onclick="playsong(${element.c[2].v})">
+      htmlCode+=`<div  class="listItem" onclick="playSong('${element.c[2].v}')">
       <img src="./img/yt.svg" class="songImg">
        <p class="songName">${element.c[1].v}</p>
       </div>`;
@@ -26,17 +26,18 @@ function playSong(vidUrl){
   let htmlCodeIntial='';
   let firstPartofUrl='https://www.youtube.com/embed/';
   let urlArr=vidUrl.split("/"); let vidId=urlArr[urlArr.length-1];let fullURL=firstPartofUrl+vidId;
-  htmlCodeIntial+=`<iframe width="550" height="350" src="${fullURL}" id="songDisplayIframe"
+  htmlCodeIntial+=`<iframe width="600" height="450" src="${fullURL}" id="songDisplayIframe"
   title="" frameborder="0"  allowfullscreen></iframe>`;
 iframeContainer.innerHTML=htmlCodeIntial;
 };
+
 
 randomSong.addEventListener('click', ()=>{
   fetch(dataSource).then(response => response.text())
   .then(jsonData => {
       let songsData = JSON.parse(jsonData.substr(47).slice(0,-2));
-      let theRandom=randomIntFromInterval(0,songsData.table.rows.length);
-      playSong(songsData.table.rows[theRandom].c[2].v);
+      let theRandom=randomIntFromInterval(0,songsData.table.rows.length-1);
+     playSong(songsData.table.rows[theRandom].c[2].v);
     });
 });
 function randomIntFromInterval(min, max) { 
@@ -48,8 +49,8 @@ layer1.classList.add("hidden");
 layer2.classList.remove("hidden");
 });
 closeLaye2.addEventListener('click',()=>{
-  layer1.classList.remove("hidden");
-  location.reload();
+layer1.classList.remove("hidden");
 layer2.classList.add("hidden");
+location.reload();
 })
 
